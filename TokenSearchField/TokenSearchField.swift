7570fdfff207de38a803e22e43ca9525cc0b29a7
Kit Cross <kit@weareunicorn.io>
2016-10-05 22:17:25 +0100

    Override initializer and add rounded corners

--- TokenSearchField/TokenSearchField.swift
+++ TokenSearchField/TokenSearchField.swift
@@ -26,4 +26,19 @@ import Cocoa
 
 class TokenSearchField: NSTextField {
   
+  required init?(coder decoder: NSCoder) {
+    super.init(coder: decoder)
+    setupSearchField()
+  }
+  
+  private func setupSearchField() {
+    wantsLayer = true
+    let tokenSearchFieldLayer = CALayer()
+    layer = tokenSearchFieldLayer
+    layer?.backgroundColor = NSColor.white.cgColor
+    layer?.borderColor = NSColor(white: 0.82, alpha: 1.0).cgColor
+    layer?.borderWidth = 1
+    layer?.cornerRadius = 5
+  }
+  
 }
